a9db4a4c3e38bd3c43f14a865ec41a1cefcacc48
Leonardo Maia Pugliese <leonardo.silva@impalastudios.com>
2022-09-10 14:28:33 +0200

    Initial Commit

---
+++ BackgroundTaskExample/BackgroundTaskExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTaskExampleApp.swift
+//  BackgroundTaskExample
+//
+//  Created by Leonardo Maia Pugliese on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTaskExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
