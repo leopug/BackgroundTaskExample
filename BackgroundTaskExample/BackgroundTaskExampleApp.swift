22cfe7a7aed5ad13055201575c3dd1a80c4d6877
Leonardo Maia Pugliese <leonardo.silva@impalastudios.com>
2022-09-13 07:59:43 +0200

    project done

--- BackgroundTaskExample/BackgroundTaskExampleApp.swift
+++ BackgroundTaskExample/BackgroundTaskExampleApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
 @main
 struct BackgroundTaskExampleApp: App {
+    @StateObject var imageStore = ImageStore()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AnyView( ContentView(imageStore: imageStore)
+                .environmentObject(imageStore)) // adding this the project will not compile anymore
         }
+        .backgroundTask(.appRefresh("randomImage")) {
+            await refreshAppData()
+        }
+    }
+    
+    func refreshAppData() async {
+        let content = UNMutableNotificationContent()
+        content.title = "A Random Photo is awaiting for you!"
+        content.subtitle = "Check it now!"
+        
+        if await fetchRandomImage() {
+            try? await UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "test", content: content, trigger: UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)))
+        }
+    }
+    
+    func fetchRandomImage() async -> Bool {
+        guard let url = URL(string: "https://picsum.photos/200/300"),
+              let (data, response) = try? await URLSession.shared.data(from: url),
+              let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            return false
+        }
+        
+        imageStore.randomImage = UIImage(data: data)
+        
+        return true
     }
 }
--- BackgroundTaskExample/ContentView.swift
+++ BackgroundTaskExample/ContentView.swift
@@ -6,21 +6,57 @@
 //
 
 import SwiftUI
+import BackgroundTasks
+
+class ImageStore: ObservableObject {
+    @Published var randomImage: UIImage?
+}
 
 struct ContentView: View {
+    
+    @ObservedObject var imageStore: ImageStore
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Button("Local Message Autorization") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                        
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }.buttonStyle(.borderedProminent)
+                .padding()
+            
+            Button("Schedule Background Task") {
+                let request = BGAppRefreshTaskRequest(identifier: "randomImage")
+                request.earliestBeginDate = Calendar.current.date(byAdding: .second, value: 5, to: Date())
+                do {
+                    try BGTaskScheduler.shared.submit(request)
+                    print("Background Task Scheduled!")
+                } catch(let error) {
+                    print("Scheduling Error \(error.localizedDescription)")
+                }
+                
+            }.buttonStyle(.bordered)
+                .tint(.red)
+                .padding()
+            
+            if let image = imageStore.randomImage {
+                Image(uiImage: image)
+            }
+            
         }
-        .padding()
     }
 }
 
+// e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"randomImage"]
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(imageStore: ImageStore())
     }
 }
